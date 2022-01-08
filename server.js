2842210cf41906ad607352df65ab3beb9e40ba6f
MiggyJ <miggyj9@gmail.com>
2022-01-08 23:44:32 +0800

    first commit

--- server.js
+++ server.js
@@ -2,13 +2,38 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+//* Multer Middleware
+const multer = require('multer')
+const path = require('path')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '/public/uploads'))
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').slice(-1)[0])
+  }
+})
+
+const upload = multer({ storage })
+//* --------------------------------------------------------------------------
+
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.send({
+    filename: req.file.originalname,
+    size: parseInt(req.file.size),
+    type: req.file.mimetype
+  })
+})
+
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
